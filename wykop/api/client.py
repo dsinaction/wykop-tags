6edb87856adab3439619bdbb953075ae7f97cfc1
dsinaction <admin@dsinaction.pl>
2021-10-05 13:46:28 +0200

    Handle new endpoints through API client

--- wykop/api/client.py
+++ wykop/api/client.py
@@ -19,13 +19,25 @@ class WykopAPI:
 
     api_url = 'https://a2.wykop.pl/'
     urls = {
-        'hits_month': '{api_url}Hits/Month/{year}/{month}/appkey/{api_key}/page/{page}/'
+        'hits_month': '{api_url}Hits/Month/{year}/{month}/appkey/{api_key}/page/{page}/',
+        'top_links': '{api_url}Links/Top/{year}/{month}/appkey/{api_key}',
+        'link_comments': '{api_url}Links/Comments/{link_id}/appkey/{api_key}',
     }
     
     def __init__(self, api_key, secret_key = None):
         self.api_key = api_key
         self.secret_key = secret_key
         self.session = requests.Session()
+
+    def get_top_links(self, year, month):
+        '''Get top links'''
+        url = self._get_url('top_links', year=year, month=month)
+        return self.get(url)
+
+    def get_comments(self, link_id):
+        '''Get link comment'''
+        url = self._get_url('link_comments', link_id=link_id)
+        return self.get(url)
 
     def get_hits_month(self, year, month, page):
         '''Get best links of selected month'''
